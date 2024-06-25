ba55f6bd457b4cefda9f77ffa5cb0b06733267a4
hurbes <arnab@doc32.in>
2024-06-26 02:09:28 +0530

    expo changes

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      extend: {
+        fontSize: {
+          dynamic: "var(--font-size-dynamic)",
+        },
+        boxShadow: {
+          card: "0 5px 20px rgba(0, 0, 0, 0.15)",
+        },
+        letterSpacing: {
+          1: "0em",
+          2: "0.025em",
+          3: "0.05em",
+          4: "0.1em",
+          5: "0.25em",
+          widest: ".25em",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
